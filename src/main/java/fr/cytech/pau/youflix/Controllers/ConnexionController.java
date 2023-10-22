1b0d7e1658d901799d1a9c4e0b9d002b85294466
Artutu64 <arthur.rimaudiere@free.fr>
2023-10-22 17:22:43 +0200

    systeme de connexion

--- src/main/java/fr/cytech/pau/youflix/Controllers/ConnexionController.java
+++ src/main/java/fr/cytech/pau/youflix/Controllers/ConnexionController.java
@@ -1,33 +1,63 @@
 package fr.cytech.pau.youflix.Controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
+import fr.cytech.pau.youflix.Models.User;
+import fr.cytech.pau.youflix.Models.Repo.UserRepository;
+import fr.cytech.pau.youflix.Utils.RandomUtil;
+import fr.cytech.pau.youflix.Utils.RedirectionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class ConnexionController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping(path = "/connexion")
-    public String connexion() {
-        return "login";
+    public String connexion(HttpServletRequest request) {
+        List<User> users = userRepository.findUserByMail("root@youfix.fr");
+        if(users.size() == 0){
+            User user = new User();
+            user.setUserId(RandomUtil.getRandomId());
+            user.setMail("root@youfix.fr");
+            user.setPassword("password");
+            user.setPrenom("root");
+            user.setNom("root");
+            userRepository.save(user);
+        }
+        return RedirectionUtil.getReturnForFacade(request.getSession(), "login");
     }
 
     @PostMapping(path = "/connexion")
-    public String postConnexion(WebRequest request) {
+    public String postConnexion(WebRequest request, HttpServletRequest request2) {
         
-        // récupération de l'adresse mail et du mot de passe
         String mail = request.getParameter("mail");
         String password = request.getParameter("password");
 
-        System.out.println("Mail : " + mail + "\nMot de passe : " + password);
-
-        // [to do] vérification que l'adresse mail existe bien dans la BDD
-
-        // [to do] vérification du mot de passe
-        
-        // les champs sont corrects, la connexion s'est bien passée
-        return "redirect:/";
+        if(mail != null && password != null){
+            List<User> users = userRepository.findAll();
+            User user = null;
+            for(User u : users){
+                if(u.getPassword().equals(password)){
+                    user = u;
+                }
+            }
+            System.out.println(users.size() + " " + mail + " " + password);
+            if(user != null){
+                HttpSession session = request2.getSession();
+                session.setAttribute("user", user);
+                return "redirect:/";
+            }
+        }
+        return "login";
 
     }
 
--- src/main/java/fr/cytech/pau/youflix/Controllers/FavorisController.java
+++ src/main/java/fr/cytech/pau/youflix/Controllers/FavorisController.java
@@ -3,12 +3,15 @@ package fr.cytech.pau.youflix.Controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fr.cytech.pau.youflix.Utils.RedirectionUtil;
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class FavorisController {
     
     @GetMapping(path = "/favoris")
-    public String favoris(){
-        return "favoris";
+    public String favoris(HttpServletRequest request){
+        return RedirectionUtil.getReturnForContent(request.getSession(), "favoris");
     }
 
 }
