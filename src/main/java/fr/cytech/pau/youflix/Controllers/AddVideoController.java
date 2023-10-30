1de6b1f42233905d9f40bbee9abe0cf8e7abadb4
hugooo225 <hugo.hrst3@gmail.com>
2023-10-30 02:13:10 +0100

    début V2 page add-actor

--- src/main/java/fr/cytech/pau/youflix/Controllers/AddVideoController.java
+++ src/main/java/fr/cytech/pau/youflix/Controllers/AddVideoController.java
@@ -21,6 +21,7 @@ import fr.cytech.pau.youflix.Models.Repo.ActeurRepository;
 import fr.cytech.pau.youflix.Models.Repo.CategorieRepository;
 import fr.cytech.pau.youflix.Models.Repo.VideoRepository;
 import fr.cytech.pau.youflix.Utils.RandomUtil;
+import fr.cytech.pau.youflix.Utils.VerifsUtil;
 
 @Controller
 public class AddVideoController {
@@ -47,101 +48,118 @@ public class AddVideoController {
 
     @PostMapping(path = "/add-video")
     public String postAddVideo(WebRequest request) throws ParseException {
+
+        /*
+         * Plan à suivre :
+         *      - [ok] récupération des informations du formulaire 
+         *      - [ok] étapes préliminaires : gestion des chaînes de caractères, etc
+         *      - [ok] vérifications des données
+         *      - [ok] si au moins une donnée est incorrecte, on renvoie l'utilisateur sur une autre page (?)
+         *      - [ok] ajout éventuel des acteurs et des genres à la base de données
+         *      - ajout du film à la base de données
+         */
         
         // récupération des informations liées à la vidéo
         String titreVideo = request.getParameter("titre-video");
-        String lienVideo = request.getParameter("lien-video");
+        String codeVideo = request.getParameter("lien-video");
         String dateSortieVideo = request.getParameter("date-sortie-video");
         String descriptionVideo = request.getParameter("description-video");
         String acteurs = request.getParameter("liste-acteurs");
         String genres = request.getParameter("liste-genres");
 
-        // set qui va contenir tous les acteurs associés au film
+        // pré-traitement des chaînes de caractères
+        dateSortieVideo = dateSortieVideo.replace("/", "-");
+        acteurs = acteurs.replace(";", ",");
+        genres = genres.replace(";", ",");
+
+        // stockage des acteurs dans un tableau
+        String[] listeActeursSplit = acteurs.split(",");
+        String[][] listeActeurs = new String[listeActeursSplit.length][2];
+        String[] nomPrenom;
+        for (int i = 0; i < listeActeursSplit.length; i++) {
+            nomPrenom = listeActeursSplit[i].split("_");
+            listeActeurs[i][0] = VerifsUtil.conversionTitleCase(nomPrenom[0]);
+            listeActeurs[i][1] = VerifsUtil.conversionTitleCase(nomPrenom[1]);
+        }
+
+        // stockage des genres dans un tableau
+        String[] listeGenres = genres.split(",");
+        for (int i = 0; i < listeGenres.length; i++) {
+            listeGenres[i] = VerifsUtil.conversionTitleCase(listeGenres[i]);
+        }
+
+        // vérification de certaines informations
+        boolean codeVideoCorrect = VerifsUtil.verifCodeVideo(codeVideo);
+        boolean dateSortieVideoCorrecte = VerifsUtil.verifDateSortieVideo(dateSortieVideo);
+
+        // si au moins une information est incorrecte, on redirige l'utilisateur vers une autre page
+        // (il ne faut pas qu'il puisse ajouter la vidéo à la base de données)
+        // note : à modifier, ne pas renvoyer la page 404 mais une autre page
+        if (!codeVideoCorrect || !dateSortieVideoCorrecte) {
+            return "404";
+        }
+
+        // vérification des acteurs liés à la vidéo
         Set<Acteur> setActeursFilm = new HashSet<>();
+        for (int i = 0; i < listeActeurs.length; i++) {
 
-        // vérification des acteurs de la vidéo et ajout éventuel à la BDD s'il/elle n'existe pas encore
-        if (acteurs != null) {
+            // récupération des informations de l'acteur
+            String nomActeur = listeActeurs[i][0];
+            String prenomActeur = listeActeurs[i][1];
 
-            // récupération des différents acteurs du film en question
-            String[] listeActeurs = acteurs.split(",");
-
-            // parcours des acteurs du film
-            for (int i=0; i<listeActeurs.length; i++) {
-
-                // suppression des espaces inutiles
-                listeActeurs[i] = listeActeurs[i].trim();
-
-                // récupération du nom et du prénom de l'acteur
-                String[] nomPrenom = listeActeurs[i].split("_");
-                String nomActeur = nomPrenom[0];
-                String prenomActeur = nomPrenom[1];
-
-                // on regarde si l'acteur existe déjà dans la BDD (acteurExiste = true) ou non (acteurExiste = false)
-                List<Acteur> acteursExistants = acteurRepository.findActorByNom(nomActeur);
-                boolean acteurExiste = false;
-                for (Acteur acteur : acteursExistants) {
-                    if (nomActeur.equals(acteur.getNom()) && prenomActeur.equals(acteur.getPrenom())) {
-                        acteurExiste = true;
-                        setActeursFilm.add(acteur);
-                    }
-                }
-
-                // si l'acteur n'existe pas encore, on l'ajoute à la BDD
-                if (!acteurExiste) {
-                    Acteur acteur = new Acteur();
-                    acteur.setIdActeur(RandomUtil.getRandomId());
-                    acteur.setNom(nomActeur);
-                    acteur.setPrenom(prenomActeur);
-                    acteurRepository.save(acteur);
+            // on regarde si l'acteur existe déjà dans la BDD (acteurExiste = true) ou non (acteurExiste = false)
+            List<Acteur> acteursExistants = acteurRepository.findActorByNom(nomActeur);
+            boolean acteurExiste = false;
+            for (Acteur acteur : acteursExistants) {
+                if (nomActeur.equals(acteur.getNom()) && prenomActeur.equals(acteur.getPrenom())) {
+                    acteurExiste = true;
                     setActeursFilm.add(acteur);
                 }
             }
-        }
 
-        // set qui va contenir tous les genres associés au film
-        Set<Categorie> setGenresFilm = new HashSet<>();
-        
-        // vérification des genres de la vidéo et ajout éventuel d'un genre à la BDD s'il n'existe pas encore
-        if (genres != null) {
-
-            // récupération des différents genres du film en question
-            String[] listeGenres = genres.split(",");
-
-            // parcours des genres du film
-            for (int i=0; i<listeGenres.length; i++) {
-
-                // suppression des espaces inutiles
-                listeGenres[i] = listeGenres[i].trim();
-
-                // on regarde si le genre existe déjà dans la BDD (genreExiste = true) ou non (genreExiste = false)
-                List<Categorie> genresExistants = genreRepository.findCategorieByNom(listeGenres[i]);
-                boolean genreExiste = false;
-                for (Categorie genre : genresExistants) { // faire plutôt un "if genresExistants == null" ?
-                    if (listeGenres[i].equals(genre.getNom())) {
-                        genreExiste = true;
-                        setGenresFilm.add(genre);
-                    }
-                }
-
-                // si le genre n'existe pas encore, on l'ajoute à la BDD
-                if (!genreExiste) {
-                    Categorie genre = new Categorie();
-                    genre.setNom(listeGenres[i]);
-                    genreRepository.save(genre);
-                    setGenresFilm.add(genre);
-                }
-
+            // si l'acteur n'existe pas encore, on l'ajoute à la BDD
+            if (!acteurExiste) {
+                Acteur acteur = new Acteur();
+                acteur.setIdActeur(RandomUtil.getRandomId());
+                acteur.setNom(nomActeur);
+                acteur.setPrenom(prenomActeur);
+                acteurRepository.save(acteur);
+                setActeursFilm.add(acteur);
             }
         }
 
+        // vérification des genres de la vidéo et ajout éventuel d'un genre à la BDD s'il n'existe pas encore
+        Set<Categorie> setGenresFilm = new HashSet<>();
+        for (int i = 0; i < listeGenres.length; i++) {
+
+            // on regarde si le genre existe déjà dans la BDD (genreExiste = true) ou non (genreExiste = false)
+            List<Categorie> genresExistants = genreRepository.findCategorieByNom(listeGenres[i]);
+            boolean genreExiste = false;
+            for (Categorie genre : genresExistants) { // faire plutôt un "if genresExistants == null" ?
+                if (listeGenres[i].equals(genre.getNom())) {
+                    genreExiste = true;
+                    setGenresFilm.add(genre);
+                }
+            }
+
+            // si le genre n'existe pas encore, on l'ajoute à la BDD
+            if (!genreExiste) {
+                Categorie genre = new Categorie();
+                genre.setNom(listeGenres[i]);
+                genreRepository.save(genre);
+                setGenresFilm.add(genre);
+            }
+
+        }
+
         // cast de la chaîne de caractères en date
-        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd-MM-yyyy");
         Date date = formatDate.parse(dateSortieVideo);
         Date dateSortieVideoSQL = new Date(date.getTime());
 
         // ajout de la vidéo à la BDD
         Video video = new Video();
-        video.setCodeVideo(lienVideo);
+        video.setCodeVideo(codeVideo);
         video.setDescription(descriptionVideo);
         video.setTitre(titreVideo);
         video.setCategories(setGenresFilm);
@@ -149,11 +167,8 @@ public class AddVideoController {
         video.setJoueDans(setActeursFilm);
         videoRepository.save(video);
 
-        // date de sortie --> champ créé sur la page mais du coup ça dépasse
-
-        return "redirect:/";
+        return "add-video";
 
     }
 
 }
-
---
+++ src/main/java/fr/cytech/pau/youflix/Utils/VerifsUtil.java
@@ -0,0 +1,49 @@
+package fr.cytech.pau.youflix.Utils;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class VerifsUtil {
+
+    // mettre une chaîne de caractères au format "title case" (une majuscule à chaque début de mot)
+    // exemple : "exemple DE Chaîne" --> "Exemple De Chaîne"
+    public static String conversionTitleCase(String chaine) {
+
+        // nouvelle chaîne, au format "title case"
+        StringBuilder nouvelleChaine = new StringBuilder();
+
+        // liste des mots de la chaîne
+        String[] listeMots = chaine.split(" ");
+
+        // parcours de chacun des mots
+        // on met la première lettre en majuscule et les autres en minuscule pour chacune d'eux
+        // on ajoute aussi un espace à la fin
+        for (String mot : listeMots) {
+            if (!mot.isEmpty()) {
+                nouvelleChaine.append(mot.substring(0, 1).toUpperCase());
+                nouvelleChaine.append(mot.substring(1).toLowerCase());
+                nouvelleChaine.append(" ");
+            }
+        }
+
+        // nouvelle chaîne finale : conversion du StringBuilder en String + suppression des espaces inutiles
+        return nouvelleChaine.toString().trim();
+    }
+    
+    // vérification du code de la vidéo
+    // format : 11 caractères parmi des majuscules, des minuscules, des tirets et des underscores
+    public static boolean verifCodeVideo(String codeVideo) {
+        Pattern pattern = Pattern.compile("[\\d\\w-]{11}");
+        Matcher matcher = pattern.matcher(codeVideo);
+        return matcher.matches();
+    }
+
+    // vérification de la date de sortie
+    // il faut une date de la forme DD-MM-YYYY en paramètre
+    public static boolean verifDateSortieVideo(String dateSortie) {
+        Pattern pattern = Pattern.compile("^(0?[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-(19\\d{2}|20\\d{2})$");
+        Matcher matcher = pattern.matcher(dateSortie);
+        return matcher.matches();
+    }
+
+}
