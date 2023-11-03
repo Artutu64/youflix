1231e83c8a74def7f5cf291d5278d5c0768ccc5a
Artutu64 <arthur.rimaudiere@free.fr>
2023-11-03 11:56:37 +0100

    ajout / edit / delete ACTEURS

--- src/main/java/fr/cytech/pau/youflix/Controllers/AdminController.java
+++ src/main/java/fr/cytech/pau/youflix/Controllers/AdminController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import fr.cytech.pau.youflix.Models.Acteur;
 import fr.cytech.pau.youflix.Models.Categorie;
+import fr.cytech.pau.youflix.Models.Repo.ActeurRepository;
 import fr.cytech.pau.youflix.Models.Repo.CategorieRepository;
 
 @Controller
@@ -15,12 +17,24 @@ public class AdminController {
 
     @Autowired
     CategorieRepository categorieRepository;
+
+    @Autowired
+    ActeurRepository acteurRepository;
     
     @GetMapping(path = "/admin")
     public String admin(Model model){
 
         List<Categorie> listeCategoriesBdd = categorieRepository.findAll();
         model.addAttribute("listeCategoriesBdd", listeCategoriesBdd);
+
+        List<Acteur> acteursBdd = acteurRepository.findAll();
+        acteursBdd.sort((a, b) -> {
+            String sa = a.getNom() + " " + a.getPrenom();
+            String sb = b.getNom() + " " + b.getPrenom();
+            return sa.compareTo(sb);
+        });
+        model.addAttribute("acteurs", acteursBdd);
+
         return "admin";
     }
 
